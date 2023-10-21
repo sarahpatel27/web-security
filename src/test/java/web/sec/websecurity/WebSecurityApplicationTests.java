2ba9f887d0820d36206edc62e177d75a32ab370a
sarahp27 <sarah.patel@xloopdigital.com>
2023-10-21 19:24:34 +0500

    code done

---
+++ src/test/java/web/sec/websecurity/WebSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package web.sec.websecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
